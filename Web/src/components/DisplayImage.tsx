33b127759334ae026f5a4bdb4ff05938dcba57e6
Łukasz Garstecki <oki@ercomp.pl>
2018-12-19 20:18:19 +0100

    Add react-canvas-draw (not working as intended)

--- Web/src/components/DisplayImage.tsx
+++ Web/src/components/DisplayImage.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { orthancURL } from "../helpers/requestHelper";
 import { connect } from "react-redux";
+import CanvasDraw from "react-canvas-draw";
 import { Button } from "@material-ui/core";
 
 export interface DisplayImageProps {
@@ -10,6 +11,14 @@ export interface DisplayImageProps {
 export interface DisplayImageState {
     readonly currentInstanceId: number;
     readonly lastScrollTop: number;
+    readonly size: Size;
+    readonly url: string;
+    readonly reload: boolean;
+}
+
+interface Size {
+    readonly width: number;
+    readonly height: number;
 }
 
 class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState> {
@@ -17,12 +26,40 @@ class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState>
         super(props);
         this.state = {
             currentInstanceId: 0,
-            lastScrollTop: 0
+            lastScrollTop: 0,
+            size: {
+                width: -1,
+                height: -1
+            },
+            url: "",
+            reload: true
         };
         console.warn(this.state);
     }
 
+    componentWillReceiveProps(props: DisplayImageProps) {
+        this.setState(prev => { return ({ reload: !prev.reload }); });
+    }
     render() {
+        const canvasProps = {
+            loadTimeOffset: 5,
+            lazyRadius: 0,
+            brushRadius: 1,
+            brushColor: "#f00",
+            catenaryColor: "#0a0302",
+            gridColor: "rgba(150,150,150,0.17)",
+            hideGrid: true,
+            canvasWidth: 400,
+            canvasHeight: 400,
+            disabled: false,
+            saveData: null,
+            immediateLoading: false
+        };
+        const url = this.props.instancesIds.length > 0 ?
+            orthancURL + "instances/" +
+            this.props.instancesIds[this.state.currentInstanceId]
+            + "/preview" :
+            "https://http.cat/404";
         return <>
             {/* <Button
                 variant="contained"
@@ -31,14 +68,11 @@ class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState>
             >
                 +
             </Button> */}
-            <img style={{ margin: "0 auto" }}
-                src={
-                    this.props.instancesIds.length > 0 ?
-                        orthancURL + "instances/" +
-                        this.props.instancesIds[this.state.currentInstanceId]
-                        + "/preview" :
-                        "https://http.cat/404"
-                }
+            {this.state.reload && <CanvasDraw
+                {...canvasProps}
+                imgSrc={url}
+                brushRadius={1}
+                style={{ margin: "0 auto" }}
                 onWheel={(e) => {
                     if (e.deltaY < 0) {
                         console.log("scrolling up");
@@ -49,6 +83,32 @@ class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState>
                         this.setState(prev => ({ currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1 }));
                     }
                 }}
+            />}
+            {!this.state.reload && <div>
+                <CanvasDraw
+                    {...canvasProps}
+                    imgSrc={url}
+                    style={{ margin: "0 auto" }}
+                    onWheel={(e) => {
+                        if (e.deltaY < 0) {
+                            console.log("scrolling up");
+                            this.setState(prev => ({ currentInstanceId: prev.currentInstanceId + 1 >= this.props.instancesIds.length ? this.props.instancesIds.length - 1 : prev.currentInstanceId + 1 }));
+                        }
+                        if (e.deltaY > 0) {
+                            console.log("scrolling down");
+                            this.setState(prev => ({ currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1 }));
+                        }
+                    }}
+                />
+            </div>}
+            <img style={{ margin: "0 auto" }}
+                src={
+                    this.props.instancesIds.length > 0 ?
+                        orthancURL + "instances/" +
+                        this.props.instancesIds[this.state.currentInstanceId]
+                        + "/preview" :
+                        "https://http.cat/404"
+                }
             />
             {/* <Button
                 variant="contained"
