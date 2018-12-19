14a411df7768ce42767183ed3d5fc57975065bb3
Łukasz Garstecki <oki@ercomp.pl>
2018-12-19 21:44:46 +0100

    Add ability to draw on image

--- Web/src/components/DisplayImage.tsx
+++ Web/src/components/DisplayImage.tsx
@@ -22,6 +22,8 @@ interface Size {
 }
 
 class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState> {
+    public saveableCanvas1: any;
+    public saveableCanvas2: any;
     constructor(props: DisplayImageProps) {
         super(props);
         this.state = {
@@ -35,22 +37,44 @@ class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState>
             reload: true
         };
         console.warn(this.state);
+        const url = props.instancesIds.length > 0 ?
+            orthancURL + "instances/" +
+            this.props.instancesIds[this.state.currentInstanceId]
+            + "/preview" :
+            "https://http.cat/404";
+        let img = new Image();
+        const fun = (w, h) => this.setState(prev => { return { size: { width: w, height: h }, reload: !prev.reload }; });
+        img.onload = function () {
+            console.log(img.naturalWidth, img.naturalHeight);
+            fun(img.naturalWidth, img.naturalHeight);
+        };
+        img.src = url;
     }
 
-    componentWillReceiveProps(props: DisplayImageProps) {
-        this.setState(prev => { return ({ reload: !prev.reload }); });
+    componentWillReceiveProps(nextProps: DisplayImageProps) {
+        const url = nextProps.instancesIds.length > 0 ?
+            orthancURL + "instances/" +
+            this.props.instancesIds[this.state.currentInstanceId]
+            + "/preview" :
+            "https://http.cat/404";
+        let img = new Image();
+        const fun = (w, h) => this.setState({ size: { width: w, height: h } });
+        img.onload = function () {
+            console.warn(img.naturalWidth, img.naturalHeight);
+            fun(img.naturalWidth, img.naturalHeight);
+        };
+        img.src = url;
+        this.setState(prev => { return { reload: !prev.reload }; });
     }
     render() {
         const canvasProps = {
             loadTimeOffset: 5,
             lazyRadius: 0,
-            brushRadius: 1,
+            brushRadius: 0,
             brushColor: "#f00",
-            catenaryColor: "#0a0302",
+            catenaryColor: "transparent",
             gridColor: "rgba(150,150,150,0.17)",
             hideGrid: true,
-            canvasWidth: 400,
-            canvasHeight: 400,
             disabled: false,
             saveData: null,
             immediateLoading: false
@@ -68,40 +92,63 @@ class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState>
             >
                 +
             </Button> */}
-            {this.state.reload && <CanvasDraw
-                {...canvasProps}
-                imgSrc={url}
-                brushRadius={1}
-                style={{ margin: "0 auto" }}
+            {this.state.reload && <div
                 onWheel={(e) => {
                     if (e.deltaY < 0) {
-                        console.log("scrolling up");
-                        this.setState(prev => ({ currentInstanceId: prev.currentInstanceId + 1 >= this.props.instancesIds.length ? this.props.instancesIds.length - 1 : prev.currentInstanceId + 1 }));
+                        console.log("div1 scrolling up");
+                        this.setState(prev => ({
+                            currentInstanceId: prev.currentInstanceId + 1 >= this.props.instancesIds.length ?
+                                this.props.instancesIds.length - 1 :
+                                prev.currentInstanceId + 1,
+                            reload: !prev.reload
+                        }));
                     }
                     if (e.deltaY > 0) {
-                        console.log("scrolling down");
-                        this.setState(prev => ({ currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1 }));
+                        console.log("div scrolling down");
+                        this.setState(prev => ({
+                            currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1,
+                            reload: !prev.reload
+                        }));
                     }
-                }}
-            />}
-            {!this.state.reload && <div>
-                <CanvasDraw
+                }}><CanvasDraw
+                    ref={canvasDraw => (this.saveableCanvas1 = canvasDraw)}
                     {...canvasProps}
                     imgSrc={url}
-                    style={{ margin: "0 auto" }}
-                    onWheel={(e) => {
-                        if (e.deltaY < 0) {
-                            console.log("scrolling up");
-                            this.setState(prev => ({ currentInstanceId: prev.currentInstanceId + 1 >= this.props.instancesIds.length ? this.props.instancesIds.length - 1 : prev.currentInstanceId + 1 }));
-                        }
-                        if (e.deltaY > 0) {
-                            console.log("scrolling down");
-                            this.setState(prev => ({ currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1 }));
-                        }
-                    }}
+                    canvasWidth={this.state.size.width}
+                    canvasHeight={this.state.size.height}
+                    style={{ margin: "0 auto", cursor: "crosshair" }}
                 />
             </div>}
-            <img style={{ margin: "0 auto" }}
+            {!this.state.reload && <div
+                onWheel={(e) => {
+                    if (e.deltaY < 0) {
+                        console.log("div scrolling up");
+                        this.setState(prev => ({
+                            currentInstanceId: prev.currentInstanceId + 1 >= this.props.instancesIds.length ?
+                                this.props.instancesIds.length - 1 :
+                                prev.currentInstanceId + 1,
+                            reload: !prev.reload
+                        }));
+                    }
+                    if (e.deltaY > 0) {
+                        console.log("div scrolling down");
+                        this.setState(prev => ({
+                            currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1,
+                            reload: !prev.reload
+                        }));
+                    }
+                }}>
+                <CanvasDraw
+                    ref={canvasDraw => (this.saveableCanvas2 = canvasDraw)}
+                    {...canvasProps}
+                    imgSrc={url}
+                    canvasWidth={this.state.size.width}
+                    canvasHeight={this.state.size.height}
+                    style={{ margin: "0 auto", cursor: "crosshair" }}
+
+                />
+            </div>}
+            {/* <img style={{ margin: "0 auto" }}
                 src={
                     this.props.instancesIds.length > 0 ?
                         orthancURL + "instances/" +
@@ -109,14 +156,40 @@ class DisplayImage extends React.Component<DisplayImageProps, DisplayImageState>
                         + "/preview" :
                         "https://http.cat/404"
                 }
-            />
-            {/* <Button
+                onWheel={(e) => {
+                    if (e.deltaY < 0) {
+                        console.log("scrolling up");
+                        this.setState(prev => ({
+                            currentInstanceId: prev.currentInstanceId + 1 >= this.props.instancesIds.length ?
+                                this.props.instancesIds.length - 1 :
+                                prev.currentInstanceId + 1,
+                            reload: !prev.reload
+                        }));
+                    }
+                    if (e.deltaY > 0) {
+                        console.log("scrolling down");
+                        this.setState(prev => ({
+                            currentInstanceId: prev.currentInstanceId - 1 < 0 ? 0 : prev.currentInstanceId - 1,
+                            reload: !prev.reload
+                        }));
+                    }
+                }}
+            /> */}
+            <Button
                 variant="contained"
                 color="primary"
-                onClick={() => this.setState(prev => ({ currentInstanceId: prev.currentInstanceId - 1 }))}
+                onClick={() => {
+                    console.log("click");
+                    localStorage.setItem(
+                        "savedDrawing",
+                        this.state.reload ? this.saveableCanvas1.getSaveData() : this.saveableCanvas2.getSaveData()
+                    );
+                    console.warn(
+                        this.state.reload ? this.saveableCanvas1.getSaveData() : this.saveableCanvas2.getSaveData());
+                }}
             >
-                -
-            </Button> */}
+                GetSavedData
+            </Button>
         </>;
     }
 }
